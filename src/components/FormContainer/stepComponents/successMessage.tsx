82e7e7b7b25a6482e9495e70b6312797cdeacc68
anna <annakoberidze1@gmail.com>
2024-08-14 22:06:51 +0400

    chore(organization): move password, username, and email components to stepComponents folder for better structure

---
+++ src/components/FormContainer/stepComponents/successMessage.tsx
@@ -0,0 +1,16 @@
+const SuccessMessage: React.FC = () => {
+  return (
+    <div className="h-full flex items-center justify-center gap-20 flex-col ">
+      <img
+        src="/src/assets/images/check-circle-fill.svg"
+        alt="success icon"
+        width="90"
+        height="90"
+      />
+      <h1 className="text-center font-medium text-blue-700">
+        თქვენ წარმატებით გაიარეთ მოცემული ფორმის საფეხურები!
+      </h1>
+    </div>
+  );
+};
+export default SuccessMessage;
--- src/components/FormContainer/successMessage.tsx
+++
@@ -1,4 +0,0 @@
-const SuccessMessage: React.FC = () => {
-  return <div>success</div>;
-};
-export default SuccessMessage;
